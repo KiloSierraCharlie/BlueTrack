58c4d0717cdeda248530dc7eaacc318e6023df7a
Kieran Cross <KiloSierraCharlie@users.noreply.github.com>
2017-04-16 14:42:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,88 @@
 #!/usr/bin/python
+from bluetooth import lookup_name
+from time import strftime, time, sleep
+import sqlite3 as sql
+from sys import exit
+from json import dumps
 
-import bluetooth
-import time
-from lcd_display import lcd
+configuration = [
 
-d = lcd()
-d.clear()
-d.display_string("Who's Home?", 1)
+  # Polling time (How often, in seconds, do you want to check who is around?)
+  
+  10,
+  
+  # Polling timeout (How long do you want the script to wait before declaring a device out-of-area?)
+  
+  3,
+  
+  #The node ID of this device (If you have multiple devices polling the same network of devices.)
+  
+  1,
+  
+]
 
-sleep_time = 600
+try:
 
+  Database = sql.connect('database.sqlite')
+  
+  Cursor = Database.cursor()
+  
+except sql.Error as Err:
+
+  print( "Error connecting to the databse!" )
+  
+  print( Err )
+  
+  exit( 1 )
+   
 while True:
-    string = ''
 
-    result = bluetooth.lookup_name('1C:66:AA:CF:DD:35', timeout=5)
-    if (result != None):
-        string = string + ' Dad'
-    d.display_string(string, 2)
+    try:
+    
+      Cursor.execute( "SELECT `macaddr`,`fname`,`lname`,`devicename` FROM `devices` WHERE 1;" )
+      
+      Devices = Cursor.fetchall()
+      
+      if( len( Devices ) == 0 ):
+      
+        print( "No devices found, waiting", configuration[ 0 ], "seconds before trying again!" )
+        
+      else:
+      
+        for device in Devices:
 
-    result = bluetooth.lookup_name('94:3A:F0:63:2D:99', timeout=5)
-    if (result != None):
-        string = string + ' Mum'
-    d.display_string(string, 2)
+          if( not lookup_name( device[ 0 ], timeout=configuration[ 1 ] ) == None ):
 
-    result = bluetooth.lookup_name('34:BB:1F:39:DE:12', timeout=5)
-    if (result != None):
-        string = string + ' Sophie'
-    d.display_string(string, 2)
+            try:
+              
+              jsonLog = dumps( [ time(), configuration[ 2 ] ] )             
 
-    f = open('whoshome.log', 'a')
-    f.write(time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime()) + string + '\n')
-    f.close()
+              Cursor.execute( "UPDATE `devices` SET `lastseen`=? WHERE `macaddr`=?", ( jsonLog, device[ 0 ], ) )
+              
+              Cursor.execute( "INSERT into `seenlog` ( `time`, `macaddr`, `nodeid` ) VALUES ( ?, ?, ? )", ( time(), device[ 0 ], configuration[ 2 ], ) )
 
-    time.sleep(sleep_time)
+              Database.commit()
+
+              if( device[2][-1] == "s" ):
+
+                  print( "%s %s' %s (%s) is in the local area!" % ( device[ 1 ], device[ 2 ], device[ 3 ], device[ 0 ] ) )
+
+              else:
+
+                  print( "%s %s's %s (%s) is in the local area!" % ( device[ 1 ], device[ 2 ], device[ 3 ], device[ 0 ] ) )
+
+            except sql.Error as Err:
+            
+              print( "There was an error updating the device information!" )
+              
+              print( Err )
+          
+    except sql.Error as Err:
+    
+      print( "Error getting devices!" )
+      
+      print( Err )
+      
+      exit( 1 )
+      
+    sleep( configuration[ 0 ] )
