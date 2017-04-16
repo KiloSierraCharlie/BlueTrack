5db3d2d3b597238d76f8287726d84c44cf1d591c
Kieran Cross <KiloSierraCharlie@users.noreply.github.com>
2017-04-16 18:12:23 +0100

    Update deviceHunter.py

--- deviceHunter.py
+++ deviceHunter.py
@@ -6,11 +6,8 @@ import bluetooth
 class DeviceHunter( bluetooth.DeviceDiscoverer ):
     
     def setDBDetails( self ):
-
         database = sql.connect('database.sqlite')
-  
         cursor = database.cursor()
-
         self.__database = database
         self.__cursor = cursor
 
@@ -30,7 +27,6 @@ class DeviceHunter( bluetooth.DeviceDiscoverer ):
             devClass = major_classes[major_class]
         else:
             devClass = "Uncategorized"
-
         self.handleUnknown( macAddr, devName, devClass )
 
     def inquiry_complete(self):
@@ -42,51 +38,29 @@ class DeviceHunter( bluetooth.DeviceDiscoverer ):
             self.__database
         except AttributeError:
             self.setDBDetails()
-
         try:
-            
             self.__cursor.execute( "SELECT `fname`, `lname`, `devicename`, `devicetype` FROM `devices` WHERE `macaddr`=?;", ( macAddr, ) )
-           
             Device = self.__cursor.fetchone()
-        
         except sql.Error as Err:
-        
             print( "Something went wrong whilst checking a new device!" )
-        
             print( Err )
 
         if( Device == None ):
-
             try:
-
                 if( devName == None ):
-
                     self.__cursor.execute( "INSERT into `devices` ( `fname`, `lname`, `macaddr`, `devicetype` ) VALUES ( 'Someone', 'Unregistered', ?, ? );", ( macAddr, devClass, ) )
-
                 else:
-
                     self.__cursor.execute( "INSERT into `devices` ( `fname`, `lname`, `devicename`, `devicetype`, `macaddr` ) VALUES ( 'Someone', 'Unregistered', ?, ?, ? );", ( devName, devClass, macAddr, ) )
-
                 self.__database.commit()
-
             except sql.Error as Err:
-
                 print( "There was an error inserting a new device to the system!" )
-
                 print( Err )
-
         else:
-
             if( not Device[ 2 ] == devName ):
-
                 self.__cursor.execute( "UPDATE `devices` SET `devicename`=? WHERE `macaddr`=?;", ( devName, macAddr, ) )
-
                 self.__database.commit()
-
             if( not Device[ 3 ] == devClass ):
-
                 self.__cursor.execute( "UPDATE `devices` SET `devicetype`=? WHERE `macaddr`=?;", ( devClass, macAddr, ) )
-
                 self.__database.commit()
 
     def hunt( self ):
